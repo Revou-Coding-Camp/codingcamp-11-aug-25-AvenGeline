6c8c6c7e6325ba4450494b7881e377bb7ec47b30
AvenGeline <yuvita.ajaaa@gmail.com>
2025-08-16 00:19:06 +0700

    Remove unused CSS file and add JavaScript functionality for user greeting and message sending

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log ("hello!");
+
+welcomespeech();
+
+//Call the function to prompt the user for their name and display a welcome message//
+function welcomespeech() {
+    let username = prompt("Please enter your name:"); 
+    alert("Welcome " + username + "! I hope you enjoy exploring my work.");
+    if (username != '') {
+       document.getElementById("user-greeting").textContent = username;
+    }
+}
+
+function sendmessage() {
+    let message = document.getElementById("message").value;
+    if (message != '') {
+        alert("Pesan berhasil dikirim: " + message);
+        document.getElementById("message").value = ''; // Clear the input field
+    } else {
+        alert("Silakan masukkan pesan sebelum mengirim.");
+    }
+}
